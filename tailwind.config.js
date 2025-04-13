99d76fad235c63d3960beb83dc7b9d76cd01e949
yooguy <ofziton@gmail.com>
2025-04-13 23:15:22 +0900

    3차

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/main/resources/templates/**/*.html"],
+    content: [
+        "./src/main/resources/templates/**/*.html",
+    ],
     theme: {
         extend: {},
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/line-clamp')
-    ],
+    plugins: [],
 }
