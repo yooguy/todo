7dcafa2406fe07e25bd18c7b0c37ef264bd5c254
yooguy <ofziton@gmail.com>
2025-04-21 11:39:55 +0900

    spinner 적용

---
+++ src/main/kotlin/com/example/todo/controller/view/ViewController.kt
@@ -0,0 +1,29 @@
+package com.example.todo.controller.view
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+
+@Controller
+@RequestMapping("/pages/guides")
+class ViewController {
+
+    @GetMapping("/common-content")
+    fun commonContent(): ModelAndView {
+        Thread.sleep(3000) // ✅ 3초간 지연 (htmx 로딩 테스트용)
+        return ModelAndView("pages/guides/common :: content")
+    }
+
+    @GetMapping("/component-content")
+    fun componentContent(): ModelAndView {
+        Thread.sleep(3000) // ✅ 3초간 지연 (htmx 로딩 테스트용)
+        return ModelAndView("pages/guides/component :: content")
+    }
+
+    @GetMapping("/design-content")
+    fun designContent(): ModelAndView {
+        Thread.sleep(3000) // ✅ 3초간 지연 (htmx 로딩 테스트용)
+        return ModelAndView("pages/guides/design :: content")
+    }
+}
